46e7b98fa05783d507bdccb428eb76986a381ab5
MetsLAtuskinsDp4-1 <58956428+ufo69@users.noreply.github.com>
2024-06-03 21:44:23 +0300

    visi faili ieladēti

---
+++ creator/ufo.php
@@ -0,0 +1,216 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>DivHub Creator</title>
+        
+	<link href="../css/bootstrap.min.css" rel="stylesheet">
+        
+	<link href="../font-awesome/css/font-awesome.min.css" rel="stylesheet"> 
+        
+	<link href="../css/creator-styles.css" rel="stylesheet">
+	
+	<link href="../css/creator-responsive.css" rel="stylesheet">
+        
+        <link href="../css/creator-portfolio.min.css" rel="stylesheet">
+        
+        <link href="../css/footer.css" rel="stylesheet">
+        
+        <link rel="shortcut icon" href="../img/favicon.png" />
+	
+</head>
+<body>
+	
+	<header>
+		<div class="container">
+			<div class="heading-wrapper">
+				<div class="row">
+					<div class="col-sm-6 col-md-6 col-lg-4">
+						<div class="info">
+							<i class="icon ion-ios-location-outline"></i>
+							<div class="right-area">
+								<h5>Riga</h5>
+								
+							</div><!-- right-area -->
+						</div><!-- info -->
+					</div><!-- col-sm-4 -->
+					
+					<div class="col-sm-6 col-md-6 col-lg-4">
+						<div class="info">
+							<i class="icon ion-ios-telephone-outline"></i>
+							<div class="right-area">
+							     <h6>Contact data</h6>
+								<h5>+371 28755655</h5>                        
+							</div><!-- right-area -->
+						</div><!-- info -->
+					</div><!-- col-sm-4 -->
+					
+					<div class="col-sm-6 col-md-6 col-lg-4">
+						<div class="info">
+							<i class="icon ion-ios-chatboxes-outline"></i>
+							<div class="right-area">
+							    <h6>Official Email</h6>
+								<h5>divhub1@gmail.com</h5>
+							</div><!-- right-area -->
+						</div><!-- info -->
+					</div><!-- col-sm-4 -->
+				</div><!-- row -->
+			</div><!-- heading-wrapper -->
+			
+		</div><!-- container -->
+	</header>
+	
+    <section class="intro-section" style="background-image: url(img/cover1.png);">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-1 col-lg-2"></div>
+				<div class="col-md-10 col-lg-8">
+					<div class="intro">
+                                            <div class="profile-img"><img src="img/cover1.png" alt=""></div>
+                                            <h2><b>Mets Latuskins</b></h2><br>
+                                            
+                                            <h4 class="font-yellow">Never gonna give you up!</h4>
+                                                <b class="text-muted">Never gonna let you down!</b>
+                                                
+                                            <br><br>
+						<ul class="social-icons">
+							<li><a href="https://github.com/" target="_blank">
+                                                                <i class="fa fa-github"></i></a></li>
+							<li><a href="https://www.linkedin.com/" target="_blank">
+                                                                <i class="fa fa-linkedin"></i></a></li>
+							<li><a href="https://web.facebook.com/" target="_blank">
+                                                                <i class="fa fa-facebook"></i></a></li>
+						</ul>
+					</div><!-- intro -->
+				</div><!-- col-sm-8 -->
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- intro-section -->
+
+        <br><br><br>
+	<section class="accordian-section section">
+		<div class="container">
+		
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="heading">
+						<h4><b>Skills</b></h4>
+					</div>
+				</div><!-- col-sm-12 -->
+				
+				<div class="col-md-12 col-lg-6">
+				
+					<div class="line-progress margin-b-20" data-prog-percent="1.00"><div></div>
+                                            <p class="progress-title"><b>Being silly</b></p></div><br>
+					
+					<div class="line-progress margin-b-20" data-prog-percent=".85"><div></div>
+                                            <p class="progress-title"><b>Procrastinating</b></p></div><br>
+
+					<div class="line-progress margin-b-20" data-prog-percent=".70"><div></div>
+                                            <p class="progress-title"><b>Reading Books</b></p></div><br>
+					
+					<div class="line-progress margin-b-50" data-prog-percent=".90"><div></div>
+                                            <p class="progress-title"><b>Bogosorting</b></p></div><br>
+
+				</div><!-- col-sm-6 -->
+				
+				<div class="col-md-12 col-lg-6">
+					<div class="panel margin-b-30">
+						<div class="panel-area margin-b-30">
+							<h5 class="panel-title active"><b>Passing Bad Puns</b>
+								<i class="icon minus ion-minus"></i><i class="icon plus ion-plus"></i></h5>
+                                                                <p class="panel-desc">How many developers does it take to screw in a lightbulb?<br>
+																None. It’s a hardware problem. <br><br>
+                                                                (cricket sounds in the background)...</p>
+						</div><!-- panel-area -->
+						
+						<div class="panel-area margin-b-30">
+							<h5 class="panel-title"><b>Web Development</b>
+								<i class="icon minus ion-minus"></i><i class="icon plus ion-plus"></i></h5>
+							<p class="panel-desc">Selfimproving everyday!</p>
+						</div><!-- panel-area -->
+						
+						<div class="panel-area margin-b-30">
+							<h5 class="panel-title"><b>Machine Learning</b>
+								<i class="icon minus ion-minus"></i><i class="icon plus ion-plus"></i></h5>
+							<p class="panel-desc">I am currently actively interested in machine learning and the use of 
+							                                                             learning and the use of AI</p>
+						</div><!-- panel-area -->
+					</div><!-- panel -->
+				</div><!-- col-sm-6-->
+				
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- accordian-section -->
+	
+	
+        
+        
+        <section class="loaders-section section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="heading">
+						<h4><b>Aptitudes</b></h4>
+					</div>
+				</div><!-- col-sm-12 -->
+				<div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="radial-prog-area margin-b-30">
+						<div class="radial-progress" data-prog-percent=".05">
+							<div></div>
+							<h6 class="progress-title">do projects on time</h6>
+						</div>
+					</div><!-- radial-prog-area-->
+				</div><!-- col-sm-6-->
+				
+				<div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="radial-prog-area margin-b-30">
+						<div class="radial-progress" data-prog-percent=".65">
+							<div></div>
+							<h6 class="progress-title">Adaptability</h6>
+						</div>
+					</div><!-- radial-prog-area-->
+				</div><!-- col-sm-6-->
+				
+				<div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="radial-prog-area margin-b-30">
+						<div class="radial-progress" data-prog-percent=".60">
+							<div></div>
+							<h6 class="progress-title">Earnest Learning</h6>
+						</div>
+					</div><!-- radial-prog-area-->
+				</div><!-- col-sm-6-->
+				
+				<div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="radial-prog-area margin-b-50">
+						<div class="radial-progress" data-prog-percent=".97">
+							<div></div>
+                                                        <h6 class="progress-title">silliness :3</h6>
+						</div>
+					</div><!-- radial-prog-area-->
+				</div><!-- col-sm-6-->
+				
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- loaders-section -->
+        
+        
+        
+        
+      
+	
+	<?php include 'assets/footer.php'; ?>
+	
+        
+	
+	<script src="../js/jquery.min.js"></script>
+	
+	<script src="../js/bootstrap.min.js"></script>
+	
+	<script src="../js/jquery.waypoints.min.js"></script>
+	
+	<script src="../js/progressbar.min.js"></script>
+	
+	<script src="../js/scripts.js"></script>
+	
+</body>
+</html>
